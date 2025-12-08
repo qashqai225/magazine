eb3645016f1461408e374506bd0b22fd04f699cc
Genest <242404530+qashqai225@users.noreply.github.com>
2025-12-08 20:19:47 +0100

    1.4 Вывод и пропись URL адресов сайта

--- main/views.py
+++ main/views.py
@@ -1,22 +1,25 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import context
 
 
+# база для "головної сторінки"
 def index(request):
     context = {
-        "title": "Головна сторінка", 
-        "content": "Главная страница магазина",
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'bool': True
-               }
+        "title": "Головна Сторінка",
+        "content": "Магазин меблів для HOME",
+    }
 
     return render(request, "main/index.html", context)
 
 
+# база для сторінки "про нас"
 def about(request):
-    return HttpResponse("About page")
+    context = {
+        "title": "Сторінка про нас",
+        "content": "Про Нас",
+        "text_on_page": "Все що потрібно ви взнаєте тут",
+    }
+
+    return render(request, "main/about.html", context)
 
 
 # Create your views here.
