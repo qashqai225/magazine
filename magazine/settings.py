63083cc53ad95e0bf8450b15e518fb6b7309d1ef
Genest <242404530+qashqai225@users.noreply.github.com>
2025-12-08 18:39:47 +0100

    1.3 Создание выводимых блоков

--- magazine/settings.py
+++ magazine/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from django.conf.global_settings import STATICFILES_DIRS
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -105,6 +107,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [BASE_DIR / 'static'] # Корневой путь статических файлов
 
 
 # Default primary key field type
